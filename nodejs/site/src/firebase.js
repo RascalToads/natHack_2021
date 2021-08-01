50c546efb14e01530621989f990ad9f0cd33fc8b
Chris Davis <chris@petal.tech>
2021-08-01 11:50:27 -0500

    Site now connects to emulator

--- nodejs/site/src/firebase.js
+++ nodejs/site/src/firebase.js
@@ -1,14 +1,17 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
-firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-});
+if (!firebase.apps.length)
+  firebase.initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  });
 
 export const db = firebase.firestore();
+console.log(process.env, process.env.USE_EMULATOR);
 
-if (process.env.USE_EMULATOR) {
-  db.useEmulator("localhost", 8080);
+if (process.env.REACT_APP_USE_EMULATOR) {
+  debugger;
+  db.useEmulator('localhost', 8080);
 }
